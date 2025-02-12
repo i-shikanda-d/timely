20bf078cd470c93f595300f661764a170f2ace1f
i-shikanda-d <i.shikanda.d@gmail.com>
2025-02-12 16:30:09 +0300

    Implement authentication with NextAuth; update navigation links and add Lifeline component

---
+++ src/app/api/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const res = await fetch("/api/auth/login", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const user = await res.json();
+        if (res.ok && user) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+});
