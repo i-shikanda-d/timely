b6f62bbc4e8619e38ae6b59a667f8dfb1140f92d
i-shikanda-d <i.shikanda.d@gmail.com>
2025-02-12 13:52:58 +0300

    Enhance navigation component with links and update homepage logo

--- src/app/components/header/nav.tsx
+++ src/app/components/header/nav.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Nav = () => {
   return (
     <div>
-      Nav
+      <ul className="flex items-center justify-between px-[10vh] py-3">
+        <Link href="/">
+          <Image src="/timely.svg" alt="timelyLogo" width={16} height={16} />
+        </Link>
+        <div className="flex space-x-10 items-center text-base font-medium">
+          <li>
+            <Link href="/anonymous">Anonymous Messages</Link>
+          </li>
+          <li>
+            <Link href="/timely">Timely</Link>
+          </li>
+        </div>
+        <div>
+          <Link
+            href="/"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          >
+            Sign In
+          </Link>
+        </div>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/bg.jpg"
-          alt="Next.js logo"
+          src="/timely.svg"
+          alt="timelyLogo"
           width={180}
           height={38}
           priority
